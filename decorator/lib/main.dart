d5a0a63b6634423e40459778b9f49cb102efc486
Kim deok hyeon <kdhmny@naver.com>
2022-01-05 23:45:44 +0900

    Decorator

---
+++ decorator/lib/main.dart
@@ -0,0 +1,17 @@
+import 'model/decorator.dart';
+
+void main() {
+  Pizza pizza1 = PizzaBase('pizza1');
+  pizza1 = Sauce(pizza1);
+  pizza1 = Mozzarella(pizza1);
+  pizza1 = Basil(pizza1);
+
+  print(pizza1.getDescription());
+
+  Pizza pizza2 = PizzaBase('pizza1');
+  pizza2 = Sauce(pizza2);
+  pizza2 = Mozzarella(pizza2);
+  pizza2 = Basil(pizza2);
+
+  print(pizza2.getDescription());
+}
